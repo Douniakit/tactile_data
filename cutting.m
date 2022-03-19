769f7b76aa5ba6b80babc859ca3a3886576b240a
Dunish <kitounii.dounia@gmail.com>
2022-03-19 18:57:30 +0100

    offsets

--- cutting.m
+++ cutting.m
@@ -37,42 +37,42 @@ clear all; close all; clc;
 % pause(0.005)
 % end
 
-D0= readtable('data/4x4_ctc_i0.csv','NumHeaderLines',1);
-D1= readtable('data/4x4_ctc_i1.csv','NumHeaderLines',1);
-D2= readtable('data/4x4_ctc_i2.csv','NumHeaderLines',1);
-
-D0=table2array(D0);
-t0=D0(:,1);
-t0=t0-t0(1);
-D0=D0(:,2:end);
-
-D1=table2array(D1);
-t1=D1(:,1);
-t1=t1-t1(1);
-D1=D1(:,2:end);
-
-D2=table2array(D2);
-t2=D2(:,1);
-t2=t2-t2(1);
-D2=D2(:,2:end);
-
-
-
-D=[D0; D1; D2];
-t1=t1+t0(end);
-t2=t2+t1(end);
-
-t_ic=[t0; t1; t2];
-id_4x4=[10, 11, 12, 13, 24, 25, 26, 27, 38, 39, 40, 41, 51, 52, 53, 54]';  
-
-D_ic=[];
-for i=1:length(id_4x4)
-        D_ic=[D_ic D(:,(id_4x4(i))*3-2:(id_4x4(i))*3)];
-end
-
-
-%save('i_contact.mat','t_ic','D_ic')
-
+% D0= readtable('data/4x4_ctc_i0.csv','NumHeaderLines',1);
+% D1= readtable('data/4x4_ctc_i1.csv','NumHeaderLines',1);
+% D2= readtable('data/4x4_ctc_i2.csv','NumHeaderLines',1);
+% 
+% D0=table2array(D0);
+% t0=D0(:,1);
+% t0=t0-t0(1);
+% D0=D0(:,2:end);
+% 
+% D1=table2array(D1);
+% t1=D1(:,1);
+% t1=t1-t1(1);
+% D1=D1(:,2:end);
+% 
+% D2=table2array(D2);
+% t2=D2(:,1);
+% t2=t2-t2(1);
+% D2=D2(:,2:end);
+% 
+% 
+% 
+% D=[D0; D1; D2];
+% t1=t1+t0(end);
+% t2=t2+t1(end);
+% 
+% t_ic=[t0; t1; t2];
+% id_4x4=[10, 11, 12, 13, 24, 25, 26, 27, 38, 39, 40, 41, 51, 52, 53, 54]';  
+% 
+% D_ic=[];
+% for i=1:length(id_4x4)
+%         D_ic=[D_ic D(:,(id_4x4(i))*3-2:(id_4x4(i))*3)];
+% end
+% 
+% 
+% %save('i_contact.mat','t_ic','D_ic')
+% 
 
 
 
--- main.m
+++ main.m
@@ -28,80 +28,93 @@ load i_contact.mat
 %% one sensor 
 
 i=6
-offset6=56257
+offset6=52260
 % the offset for fx fy fz are different filter arbitrary values
 
 
 fx6_nc=D_nc(:,(i-1)*3+1);
+offsetx6=mean(fx6_nc);
+fx6_nc=fx6_nc-offsetx6;
+
+
 fy6_nc=D_nc(:,(i-1)*3+1);
+offsety6=mean(fy6_nc);
+fy6_nc=fy6_nc-offsety6;
+
 fz6_nc=D_nc(:,(i-1)*3+1);
+offsetz6=mean(fz6_nc);
+fz6_nc=fz6_nc-offsetz6;
+
 f6_nc=sqrt(fx6_nc.^2+fy6_nc.^2+fz6_nc.^2);
 
-idx=find(f6_nc==0);
+idx=find(f6_nc>offset6);
 for k=1:length(idx)
-    f6_nc(idx(k))=offset6;    
+    f6_nc(idx(k))=3982.43;    
 end
 
 
-fx6_sc=D_sc(:,(i-1)*3+1);
-fy6_sc=D_sc(:,(i-1)*3+1);
-fz6_sc=D_sc(:,(i-1)*3+1);
+fx6_sc=D_sc(:,(i-1)*3+1)-offsetx6;
+fy6_sc=D_sc(:,(i-1)*3+1)-offsety6;
+fz6_sc=D_sc(:,(i-1)*3+1)-offsetz6;
 f6_sc=sqrt(fx6_sc.^2+fy6_sc.^2+fz6_sc.^2);
 
-idx=find(f6_sc==0);
+idx=find(f6_sc>offset6);
 for k=1:length(idx)
-    f6_sc(idx(k))=offset6;    
+    f6_sc(idx(k))=4032.66;    
 end
 
-fx6_ic=D_ic(:,(i-1)*3+1);
-fy6_ic=D_ic(:,(i-1)*3+1);
-fz6_ic=D_ic(:,(i-1)*3+1);
+fx6_ic=D_ic(:,(i-1)*3+1)-offsetx6;
+fy6_ic=D_ic(:,(i-1)*3+1)-offsety6;
+fz6_ic=D_ic(:,(i-1)*3+1)-offsetz6;
 f6_ic=sqrt(fx6_ic.^2+fy6_ic.^2+fz6_ic.^2);
 
-idx=find(f6_ic==0);
+idx=find(f6_ic>offset6);
 for k=1:length(idx)
-    f6_ic(idx(k))=offset6;    
+    f6_ic(idx(k))=3987.63;    
 end
-% figure,
-% subplot(4,1,1),plot(t_nc,fx6_nc,'r'),xlabel('t'),ylabel('Fx'),hold on, plot(t_sc,fx6_sc,'g'),hold on, plot(t_ic,fx6_ic,'b'),
-% subplot(4,1,2),plot(t_nc,fy6_nc,'r'),xlabel('t'),ylabel('Fy'),hold on, plot(t_sc,fy6_sc,'g'),hold on, plot(t_ic,fy6_ic,'b'),
-% subplot(4,1,3),plot(t_nc,fz6_nc,'r'),xlabel('t'),ylabel('Fz'),hold on, plot(t_sc,fz6_sc,'g'),hold on, plot(t_ic,fz6_ic,'b'),
-% subplot(4,1,4),plot(t_nc,f6_nc,'r'),xlabel('t'),ylabel('F'),hold on, plot(t_sc,f6_sc,'g') ,hold on, plot(t_ic,f6_ic,'b'),
+figure,
+subplot(4,1,1),plot(t_nc,fx6_nc,'r'),xlabel('t'),ylabel('Fx'),hold on, plot(t_sc,fx6_sc,'g'),hold on, plot(t_ic,fx6_ic,'b'),
+subplot(4,1,2),plot(t_nc,fy6_nc,'r'),xlabel('t'),ylabel('Fy'),hold on, plot(t_sc,fy6_sc,'g'),hold on, plot(t_ic,fy6_ic,'b'),
+subplot(4,1,3),plot(t_nc,fz6_nc,'r'),xlabel('t'),ylabel('Fz'),hold on, plot(t_sc,fz6_sc,'g'),hold on, plot(t_ic,fz6_ic,'b'),
+subplot(4,1,4),plot(t_nc,f6_nc,'r'),xlabel('t'),ylabel('F'),hold on, plot(t_sc,f6_sc,'g') ,hold on, plot(t_ic,f6_ic,'b'),
 
 
  figure,plot(t_nc,f6_nc,'r'),xlabel('t'),ylabel('F'),hold on, plot(t_sc,f6_sc,'g') ,hold on, plot(t_ic,f6_ic,'b'),
-
-
+ 
+T=[t_nc;t_sc+t_nc(end);t_ic+t_nc(end)+t_sc(end)]
+FF=[f6_nc;f6_sc;f6_ic]
+figure,plot(T,FF,'r'),xlabel('t'),ylabel('F')
 
 % td=t_nc(2:end)-t_nc(1:(end-1))
 % figure,plot(td,'.')
 
-t_s=0.001;
-i=1;
-t_sc_d=t_sc(i);
-f_sc_d=f6_sc(i);
-
-
-for k=2:length(t_sc)
-    d=t_sc(k)-t_sc_d(i);
-    if (d<=t_s)
-        i=i+1;
-        t_sc_d(i)=t_sc_d(i-1)+t_s;
-        f_sc_d(i)=f6_sc(k);
-    else 
-        n=fix(d/t_s);
-        for j=1:n
-            i=i+1;
-            t_sc_d(i)=t_sc_d(i-1)+t_s;
-            f_sc_d(i)=f6_sc(k);            
-        end        
-    end
-    
-end
-
-figure,plot(t_sc,f6_sc,'r'),xlabel('t'),ylabel('F'),hold on, plot(t_sc_d,f_sc_d,'m')% ,hold on, plot(t_ic,f6_ic,'b'),
-
-
+% t_s=0.001;
+% i=1;
+% t_sc_d=t_sc(i);
+% f_sc_d=f6_sc(i);
+% 
+% 
+% for k=2:length(t_sc)
+%     d=t_sc(k)-t_sc_d(i);
+%     if (d<=t_s)
+%         i=i+1;
+%         t_sc_d(i)=t_sc_d(i-1)+t_s;
+%         f_sc_d(i)=f6_sc(k);
+%     else 
+%         n=fix(d/t_s);
+%         for j=1:n
+%             i=i+1;
+%             t_sc_d(i)=t_sc_d(i-1)+t_s;
+%             f_sc_d(i)=f6_sc(k);            
+%         end        
+%     end
+%     
+% end
+% 
+% figure,plot(t_sc,f6_sc,'r'),xlabel('t'),ylabel('F'),hold on, plot(t_sc_d,f_sc_d,'m')% ,hold on, plot(t_ic,f6_ic,'b'),
+% 
+% 
+% plot(f6_ic)
 
 
 
